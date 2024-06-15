48ef3ee61f22171a7ecb7a2f5236e83f04907d1b
lorransantosdev <lorransantossarmentol@gmail.com>
2024-06-14 22:12:55 -0300

    variaveis para login criadas

---
+++ public/javascript/script.js
@@ -0,0 +1,19 @@
+function logar() {
+    var usuario = document.getElementById("usuario").value;
+    var senha = document.getElementById("senha").value;
+    
+    var usuarioTrue = "Caio";
+    var senhaTrue = "Caio123";
+
+    if (usuario !== usuarioTrue || senha !== senhaTrue) {
+        alert("Você inseriu usuário ou senha incorretos");
+        document.getElementById("usuario").value = "";
+        document.getElementById("senha").value = "";
+        
+    } else {
+        alert(`Login realizado com sucesso ${usuario}. Mergulhe no universo das corridas sustentáveis!`);
+        window.location.href = "/src/pages/index/index.html";
+    }
+    
+
+}
